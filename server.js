e9c1e33eafe4fab5ccd62a1ac659ba9605071b0d
prof-naranjo <73539896+prof-naranjo@users.noreply.github.com>
2020-11-09 21:46:07 -0500

    Fixed the merge

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // first node file for GitHub
 console.log("Hello CIS485 Fall 2020 Semester");
-console.log("Changed by Prof. Naranjo"); 
+console.log("Changed by Prof. Naranjo");
+console.log("The issue was with my merge locally");
 // comment form my mac at 8:45pm
