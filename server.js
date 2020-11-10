7dea429c5ab61121d9c122e28ba51665da2bcc39
prof-naranjo <73539896+prof-naranjo@users.noreply.github.com>
2020-11-09 21:44:33 -0500

    Added second console.log

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 // first node file for GitHub
 console.log("Hello CIS485 Fall 2020 Semester");
-
-// comment form my mac at 8:45pm, 9:02pm by chmieres!!!!!!!
+console.log("Changed by Prof. Naranjo"); 
+// comment form my mac at 8:45pm
