18e6814a5080aae196f699342132d4b28073fc79
prof-naranjo <73539896+prof-naranjo@users.noreply.github.com>
2020-11-10 07:46:40 -0500

    Commit Tue

--- server.js
+++ server.js
@@ -2,5 +2,5 @@
 console.log("Hello CIS485 Fall 2020 Semester");
 console.log("Changed by Prof. Naranjo");
 console.log("The issue was with my merge locally");
-console.log("Fixed frrom Visual Studio Code");
+console.log("Fixed frrom Visual Studio Code"); 
 // comment form my mac at 8:45pm
