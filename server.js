da418eda29a936646507a56d3f3798f4532c844a
Christian Mieres <chmieres6@gmail.com>
2020-11-09 21:12:49 -0500

    Added my signature chmieres

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 // first node file for GitHub
 console.log("Hello CIS485 Fall 2020 Semester");
 
-// comment form my mac at 8:45pm, 9:02pm
+// comment form my mac at 8:45pm, 9:02pm by chmieres!!!!!!!
